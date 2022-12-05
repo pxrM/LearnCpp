3ba9f33d1e3ee1c1128ffca0b9e2a7fde79f9bbe
pengxingran <pxr680@163.com>
2022-12-05 11:23:31 +0800

    友元类、友元函数

--- MyCppProject/5.friend/5.friend.cpp
+++ MyCppProject/5.friend/5.friend.cpp
@@ -1,3 +1,78 @@
 /*
-	友元类、友元函数xxxxxxxx
+	友元类、友元函数
 */
+
+#include <stdio.h>
+class TestClass {
+	friend class Test2; //Test2类可以访问本类的所有信息
+public:
+	void TestFun(){}
+
+private:
+	void TestFun1() {}
+	void TestFun2() {}
+	void TestFun3() {}
+
+protected:
+	void TestFun4(){}
+};
+
+
+class Test2 {
+public:
+	void Init() {
+		testClass.TestFun();
+		// 利用友元类访问私有函数
+		testClass.TestFun1();
+		testClass.TestFun2();
+	}
+
+private:
+	TestClass testClass;
+};
+
+
+class Test3 {
+public:
+	/// <summary>
+	/// 友元函数（没有this指针 ） 可以访问该类的所有成员，
+	///		不能使用const、不能被继承
+	///		提高了程序的运行效率、破坏了类的封装和隐蔽性
+	/// </summary>
+	/// <param name="t3"></param>
+	friend void Printf_F(Test3& t3) /*const*/ {
+		t3.Print();
+		printf("%d \n", t3.b);
+		printf("%d \n", t3.a);
+	}
+
+	static void Printf_S(Test3& t3) {
+		t3.Print();
+		printf("%d \n", t3.b);
+		printf("%d \n", t3.a);
+	}
+
+private:
+	void Print() {
+		a = 0;
+		b = 10;
+	}
+
+private:
+	int a;
+	int b;
+};
+
+
+
+int main() {
+	Test2 test2;
+	test2.Init();
+
+
+	Test3 t3;
+	Printf_F(t3);
+	Test3::Printf_S(t3);
+
+	return 0;
+}
