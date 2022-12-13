8985b9abec4979e9f39cbb356ef93b46175472ab
pengxingran <pxr680@163.com>
2022-12-13 15:12:26 +0800

    io 拷贝

--- MyCppProject/14.io/14.io.cpp
+++ MyCppProject/14.io/14.io.cpp
@@ -9,6 +9,44 @@
 using namespace std;
 
 
+
+void FileRead(const char *inflilename, char *inbuffer)
+{
+	ifstream readFile;
+	readFile.open(inflilename);
+	readFile.seekg(0, ios::end);
+	int filelen = readFile.tellg();
+	readFile.seekg(0, ios::beg);
+	readFile.read(inbuffer, filelen);
+	cout << inbuffer << endl;
+	readFile.close();
+}
+
+bool FileCopy(const char *inCopyFileName, const char *inTargetFileName)
+{
+	char buffer[4096] = { 0 };
+	ifstream ReadFile;
+	ofstream WriteFile;
+	ReadFile.open(inCopyFileName);
+	WriteFile.open(inTargetFileName);
+
+	//while (ReadFile >> buffer)
+	//{
+	//	cout << buffer << endl;
+	//	WriteFile << buffer << endl;
+	//}
+	ReadFile.seekg(0, ios::end);
+	int filelen = ReadFile.tellg();
+	ReadFile.seekg(0, ios::beg);
+	ReadFile.read(buffer, filelen);
+	WriteFile << buffer;
+
+	ReadFile.close();
+	WriteFile.close();
+
+	return true;
+}
+
 int main()
 {
 	cout << "iostream" << endl;
@@ -26,12 +64,12 @@ int main()
 	//ofstream //创建文件流并输入信息
 	//ifstream //输出文件流
 	//fstream //创建文件、写入信息、读取信息
-	ofstream file;
-	file.open("如果不存在文件会自动创建.txt", ios::app); // ios::app 以追加的方式写入，而不是覆盖
+	ofstream writefile;
+	writefile.open("如果不存在文件会自动创建.txt", ios::app); // ios::app 以追加的方式写入，而不是覆盖
 	cout << "开始写入信息" << endl;
 	cin.getline(buffer, 1024);
-	file << buffer << endl;
-	file.close();
+	writefile << buffer << endl;
+	writefile.close();
 
 	ifstream readfile;
 	readfile.open("如果不存在文件会自动创建.txt");
@@ -48,6 +86,24 @@ int main()
 	readfile.read(buffer2, filelen); //读取数据
 	cout << buffer2 << endl;
 	readfile.close();
+
+	/*
+		ios::app   追加写入
+		ios::ate   打开文件时游标自动定位到文件末尾
+		ios::in    读取
+		ios::out   写入
+		ios::trunc 截断，如果文件存在打开时进行截断，文件长度设置为0
+	*/
+	fstream file;
+	file.open("如果不存在文件会自动创建.txt", ios::out | ios::in);
+	file << "xxxxxxxxxx" << endl;
+	file.close();
+	char buffer3[1024] = { 0 };
+	FileRead("如果不存在文件会自动创建.txt", buffer3);
+
+	FileCopy("14.io.cpp", "拷贝C屁屁.cpp");
+
+
 ;
 	return 0;
 }
