085c50c31f0cec1f1734ef5d7952e738b7021464
pengxingran <pxr680@163.com>
2023-02-19 17:16:36 +0800

    使用dynamic_pointer_cast强转指针

--- MyCppProject/19.Ptr/19.Ptr.cpp
+++ MyCppProject/19.Ptr/19.Ptr.cpp
@@ -81,9 +81,9 @@ public:
 	{
 	}
 
-	IInterface *CreateInterface()
+	IInterface* CreateInterface()
 	{
-		IInterface *face = new B();
+		IInterface* face = new B();
 		face->SetPtr(CShared);
 		return face;
 	}
@@ -111,7 +111,7 @@ public:
 	{}
 
 	template<class T>
-	void operator()(T *p)
+	void operator()(T* p)
 	{
 		cout << "operator()" << endl;
 		delete p;
@@ -123,7 +123,7 @@ public:
 int main()
 {
 	// 裸指针
-	int *iPtr = new int;
+	int* iPtr = new int;
 
 	// 共享指针	std::shared_ptr
 	std::shared_ptr<int>  siPtr1(iPtr);
@@ -133,14 +133,14 @@ int main()
 	cout << "当前引用计数=" << siPtr1.use_count() << endl;
 	siPtr4 = nullptr;	//减少引用计数	不会清除对象 除非计数为0了
 	cout << "当前引用计数=" << siPtr1.use_count() << endl;
-	int *iPtr2 = siPtr1.get();	//转为原始指针（裸指针）
+	int* iPtr2 = siPtr1.get();	//转为原始指针（裸指针）
 	int ivalue = *siPtr1;	//获取值
 	bool bSingle = siPtr1.unique(); //是不是唯一的
 	//siPtr1.swap(siPtr2);	//交换
 	siPtr1.reset();	//reset = (siPtr1 = nullptr)
 
 	shared_ptr<CTestPtr> ctsptr1(new CTestPtr);
-	CTestPtr *ctptr = ctsptr1.get();	//转裸指针
+	CTestPtr* ctptr = ctsptr1.get();	//转裸指针
 	shared_ptr<CTestPtr> ctsptr2 = ctptr->shared_from_this();	//转共享指针
 
 	// 弱指针 std::weak_ptr		UE4：TWeakPtr
@@ -154,7 +154,7 @@ int main()
 
 
 	ATest ATest1;
-	IInterface *ii = ATest1.CreateInterface();
+	IInterface* ii = ATest1.CreateInterface();
 	ii->DoWork();
 
 	/*
@@ -165,9 +165,9 @@ int main()
 	auto_ptr<CTestPtr> autop1(new CTestPtr());
 	auto_ptr<CTestPtr> autop2;
 	autop2 = autop1;	//autop1=null 会剥夺autop1的控制权，容易导致内存奔溃
-	CTestPtr *cc1 = autop2.release();
+	CTestPtr* cc1 = autop2.release();
 	autop2.reset(new CTestPtr());	//重新设置一个类
-	CTestPtr *cc2 = autop2.get();
+	CTestPtr* cc2 = autop2.get();
 
 	unique_ptr<CTestPtr> autop3(new CTestPtr());	//唯一、没有引用计数、不能拷贝
 	unique_ptr<CTestPtr> autop4 = unique_ptr<CTestPtr>(new CTestPtr());
@@ -190,6 +190,37 @@ int main()
 	MWeakPtr<CTestPtr2>TestPtr6(TestPtr2);
 	TestPtr6.Pin()->Init();
 	TestPtr6.Release();
+
+
+
+
+	struct CastTestA :public enable_shared_from_this<CastTestA>
+	{
+		CastTestA()
+			:a(100)
+		{}
+
+		//使用dynamic_pointer_cast必须保证至少有个一个虚函数
+		virtual void Test()
+		{}
+
+		int a;
+	};
+	struct CastTestB :public CastTestA
+	{
+		CastTestB()
+			:b(200)
+		{}
+
+		int b;
+	};
+
+	shared_ptr<CastTestA> MA(new CastTestB);
+	cout << MA->a << endl;
+	//MA->b;	//无法访问需要强转
+	shared_ptr<CastTestB>MB = dynamic_pointer_cast<CastTestB>(MA);
+	cout << MB->b << endl;
+
 
 	return 0;
 }
